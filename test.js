33859be2e4f0f63e90c7beeb80a49270c319d53c
austinbillings <austin@austinbillings.com>
2019-02-26 18:31:55 -0500

    test.js: use simpler apprach for tests with eval-based caller fns

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
 const zaq = require('zaq');
 const path = require('path');
+const chalk = require('chalk');
 const colleqtor = require('./index');
 
-let testDir = path.join(__dirname, '/test');
+const runTest = (code) => zaq.ok(chalk.green(code), eval(code));
+const runAsyncTest = (code) => eval(code).then(results => zaq.ok(chalk.green(code), results));
 
+const testDir = path.join(__dirname, '/test');
 
-zaq.ok('LIST FILES: Only dir provided:', colleqtor.listFiles(testDir));
-zaq.ok('lsitFiles: Dir and extension (log) provided:', colleqtor.listFiles(testDir, { extension: 'log' }));
-zaq.ok('listFiles: Dir and extension (txt) provided:', colleqtor.listFiles(testDir, { extension: 'txt' }));
-zaq.ok('listFiles: Dir and extensions (log, txt) provided:', colleqtor.listFiles(testDir, { extension: ['log', 'txt'] }));
-zaq.ok('listFiles: Dir and extension (txt) provided, stripping directory:', colleqtor.listFiles(testDir, { extension: 'txt', stripDirPath: true }));
-zaq.ok('gatherFileNames: Only dir provided:', colleqtor.gatherFileNames(testDir));
-zaq.ok('.requireAll(testDir + \'/requireable\')', colleqtor.requireAll(testDir + '/requirable'));
-zaq.ok('getFileContent(listFiles(testDir, \'txt\'))', colleqtor.getFileContents(colleqtor.listFiles(testDir, { extension: 'txt' })));
-colleqtor.listFiles(testDir, { extension: 'txt', useAsync: true })
-    .then(list => zaq.ok('listFiles with { useAsync: true }', list));
+runTest(`colleqtor.listFiles(testDir)`);
+runTest(`colleqtor.listFiles(testDir, { extension: 'txt', stripDirPath: true })`);
+runTest(`colleqtor.listFiles(testDir, { extension: ['log', 'txt'] })`);
+runTest(`colleqtor.listFiles(testDir, { extension: 'txt', stripDirPath: true })`);
+runTest(`colleqtor.listFiles(testDir, { directories: true, recursive: true, stripDirPath: true })`);
+runTest(`colleqtor.gatherFileNames(testDir)`);
+runTest(`colleqtor.requireAll(testDir + '/requirable')`);
+runTest(`colleqtor.getFileContents(colleqtor.listFiles(testDir, { extension: 'txt' }))`);
+runAsyncTest(`colleqtor.listFiles(testDir, { extension: 'txt', useAsync: true })`);
+runAsyncTest(`colleqtor.listFiles(testDir, { directories: true, useAsync: true, extension: 'log', recursive: true })`);
